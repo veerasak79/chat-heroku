d6c25d31d4fcfda6dd5eebd33c44f14ac8a0e20f
veerasak79 <71983914+veerasak79@users.noreply.github.com>
2020-09-28 01:31:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const io = require("socket.io")();
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3002
 
-let currentUserId = 2;
-let currentMessageId = 1;
-const userIds = {};
 
-const PORT = process.env.PORT || 3002;
+app.get("/", (req, res)=>{
+    res.json({result: "ok", data:[1,2,3,4,5]})
+})
 
-function createMessage(userId, messageText) {
-  return {
-    _id: currentMessageId++,
-    text: messageText,
-    createdAt: new Date(),
-    user: {
-      _id: userId,
-      name: "Test User",
-      avatar: "https://placeimg.com/140/140/any",
-    },
-  };
-}
-
-io.on("connection", (socket) => {
-  console.log("a user connected!");
-  console.log(socket.id);
-  userIds[socket.id] = currentUserId++;
-
-  socket.on("message", (messageText) => {
-    //console.log(messageText);
-    // io.emit("message", messageText );
-    const userId = userIds[socket.id];
-    const message = createMessage(userId, messageText);
-    //console.log(message);
-    socket.broadcast.emit("message", message);
-  });
-});
-
-io.listen(PORT, () => {
-  console.log(`running port. ${PORT}`)
-});
+app.listen(PORT, ()=>{
+    console.log(`Serer is running. ${PORT}`)
+})
