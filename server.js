41b7400d11cfca64f65e4d6243d2dd0d2e101c6b
veerasak79 <71983914+veerasak79@users.noreply.github.com>
2020-09-28 12:47:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const express = require("express")
-const app = express()
+// check port on server
+//heroku logs --tail --app livechatbig 
+
+
+const io = require("socket.io")();
+
+let currentUserId = 2;
+let currentMessageId = 1;
+const userIds = {};
+
 const PORT = process.env.PORT || 3002
 
+function createMessage(userId, messageText) {
+  return {
+    _id: currentMessageId++,
+    text: messageText,
+    createdAt: new Date(),
+    user: {
+      _id: userId,
+      name: "Test User",
+      avatar: "https://placeimg.com/140/140/any",
+    },
+  };
+}
 
-app.get("/", (req, res)=>{
-    res.json({result: "ok", data:[1,2,3,4,5]})
-})
+io.on("connection", (socket) => {
+  console.log("a user connected!");
+  console.log(socket.id);
+  userIds[socket.id] = currentUserId++;
 
-app.listen(PORT, ()=>{
-    console.log(`Serer is running. ${PORT}`)
+  socket.on("message", (messageText) => {
+    //console.log(messageText);
+    // io.emit("message", messageText );
+    const userId = userIds[socket.id];
+    const message = createMessage(userId, messageText);
+    //console.log(message);
+    socket.broadcast.emit("message", message);
+  });
+});
+
+
+io.listen(PORT, ()=>{
+  console.log(`Serer is running. ${PORT}`)
 })
